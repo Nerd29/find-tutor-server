76da4d5e444ac685d91e29713005eddf66361cc9
Nerd29 <majumderturja59@gmail.com>
2026-07-26 02:36:29 +0600

    get tutors data(all,id) and post booking session to server done

---
+++ index.js
@@ -0,0 +1,78 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 8000;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+
+//kohRYfeEcDaC7yTK
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+
+  
+    
+    
+    const database = client.db("find-tutor");
+    const tutorsCollection = database.collection("tutor-list");
+    const bookingCollections=database.collection('bookings')
+    // const result = await tutorsCollection.find({}).toArray();
+    app.get('/tutors', async (req, res) => {
+      const result = await tutorsCollection.find({}).toArray();
+      
+      res.send(result);
+
+      
+    });
+
+   
+
+    app.get('/tutors/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await tutorsCollection.findOne(query);
+        res.send(result);
+      });
+
+       app.post('/booking',async(req,res)=>{
+      const bookingData=req.body;
+      const result=await bookingCollections.insertOne(bookingData)
+
+      res.json(result)
+    })
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
